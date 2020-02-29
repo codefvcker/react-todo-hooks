1343f06bcff26e78b85d25c995aad33ab3a20028
codefvcker <alehsiarheyeu@gmail.com>
2020-02-29 14:55:52 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
-import {TodoPage} from './components/TodoPage'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <TodoPage />
     </div>
   );
